c68404b8e500719e021f3533dfb4548e38920cf6
Jersonnn <jerson.cerezo.100@gmail.com>
2025-10-27 07:53:10 +0800

    check if table exist then when yes remigrate when not add to migrate

--- database/migrations/2025_10_24_005407_create_positions_table.php
+++ database/migrations/2025_10_24_005407_create_positions_table.php
@@ -11,7 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('positions', function (Blueprint $table) {
+        if (!Schema::hasTable('positions')) {
+            Schema::create('positions', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique(); // Position name (e.g., "Manager", "Software Engineer")
             $table->string('code')->unique(); // Position code (e.g., "MGR", "SE", "RAF")
@@ -30,6 +31,7 @@ return new class extends Migration
             $table->index(['is_active', 'department_id']);
             $table->index('level');
         });
+        }
     }
 
     /**
